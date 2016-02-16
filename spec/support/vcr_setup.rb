68d7b69b1c603e99481aa009241738ba201e08f0
ToniRib <tonimarierib@gmail.com>
2016-02-16 13:14:33 -0700

    Add allow http connections when no cassette to VCR setup

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,6 +1,5 @@
 VCR.configure do |config|
-  #the directory where your cassettes will be saved
   config.cassette_library_dir = "spec/vcr"
-  # your HTTP request service. You can also use fakeweb, webmock, and more
   config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
 end
