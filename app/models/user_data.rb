9e781b9df7053be22429cb0dabce45d5aa114f2d
ToniRib <tonimarierib@gmail.com>
2016-02-17 15:10:03 -0700

    Add tests for UserData class

--- app/models/user_data.rb
+++ app/models/user_data.rb
@@ -24,22 +24,18 @@ class UserData
     all_heartrates.last
   end
 
-  def todays_sleep
-    sleep.last
-  end
-
   def heartrate_chart
     LazyHighCharts::HighChart.new("basic_line") do |f|
-      f.chart({ type: "line",
-                marginRight: 130,
-                marginBottom: 25 })
-      f.yAxis({ title: { text: "Beats per Minute" } })
+      f.chart(type: "line",
+              marginRight: 130,
+              marginBottom: 25 )
+      f.yAxis(title: { text: "Beats per Minute" })
       f.xAxis(title: { text: "Date" },
               type: "category",
               labels: {enabled: false})
-      f.tooltip({ valueSuffix: "bpm" })
+      f.tooltip(valueSuffix: "bpm")
       f.legend(enabled: false)
-      f.series({ name: "Resting Heart Rate", data: heartrate_points })
+      f.series(name: "Resting Heart Rate", data: heartrate_points)
     end
   end
 end
--- spec/models/user_data_spec.rb
+++ spec/models/user_data_spec.rb
@@ -2,21 +2,6 @@ require "spec_helper"
 
 RSpec.describe UserData, type: :model do
   it "has valid attributes" do
-    friends, sleep, heartrate, last_nights_sleep, user_data = build_user_data
-
-    expect(user_data.friends).to eq(friends)
-    expect(user_data.sleep).to eq(sleep)
-    expect(user_data.heartrate).to eq(heartrate)
-    expect(user_data.last_nights_sleep).to eq(last_nights_sleep)
-  end
-
-  it "#all_heartrates returns array of heartrates" do
-    friends, sleep, heartrate, last_nights_sleep, user_data = build_user_data
-
-    expect(user_data.all_heartrates).to eq([84, 84])
-  end
-
-  def build_user_data
     friends = build_list(:friend, 2)
     sleep = build_list(:sleep, 2)
     heartrate = build_list(:heartrate, 2)
@@ -27,6 +12,71 @@ RSpec.describe UserData, type: :model do
                              heartrate:         heartrate,
                              last_nights_sleep: last_nights_sleep)
 
-    [friends, sleep, heartrate, last_nights_sleep, user_data]
+    expect(user_data.friends).to eq(friends)
+    expect(user_data.sleep).to eq(sleep)
+    expect(user_data.heartrate).to eq(heartrate)
+    expect(user_data.last_nights_sleep).to eq(last_nights_sleep)
+  end
+
+  it "#all_heartrates returns array of heartrates" do
+    friends = build_list(:friend, 2)
+    sleep = build_list(:sleep, 2)
+    heartrates = [build(:heartrate, rate: 84), build(:heartrate, rate: 88)]
+    last_nights_sleep = build(:last_nights_sleep)
+
+    user_data = UserData.new(friends:           friends,
+                             sleep:             sleep,
+                             heartrate:         heartrates,
+                             last_nights_sleep: last_nights_sleep)
+
+    expect(user_data.all_heartrates).to eq([84, 88])
+  end
+
+  it "#heartrate_points returns array of heartrate date and rate hashes" do
+    friends = build_list(:friend, 2)
+    sleep = build_list(:sleep, 2)
+    heartrates = [build(:heartrate, rate: 84, date: "2016-02-16"),
+                  build(:heartrate, rate: 88, date: "2016-02-17")]
+    last_nights_sleep = build(:last_nights_sleep)
+
+    user_data = UserData.new(friends:           friends,
+                             sleep:             sleep,
+                             heartrate:         heartrates,
+                             last_nights_sleep: last_nights_sleep)
+
+    points = [
+      { name: "2016-02-16", y: 84 },
+      { name: "2016-02-17", y: 88 }
+    ]
+
+    expect(user_data.heartrate_points).to eq(points)
+  end
+
+  it "#minimum_heartrate returns minimum rate" do
+    friends = build_list(:friend, 2)
+    sleep = build_list(:sleep, 2)
+    heartrates = [build(:heartrate, rate: 84), build(:heartrate, rate: 88)]
+    last_nights_sleep = build(:last_nights_sleep)
+
+    user_data = UserData.new(friends:           friends,
+                             sleep:             sleep,
+                             heartrate:         heartrates,
+                             last_nights_sleep: last_nights_sleep)
+
+    expect(user_data.minimum_heartrate).to eq(84)
+  end
+
+  it "#todays_heartrate returns last heartrate" do
+    friends = build_list(:friend, 2)
+    sleep = build_list(:sleep, 2)
+    heartrates = [build(:heartrate, rate: 84), build(:heartrate, rate: 88)]
+    last_nights_sleep = build(:last_nights_sleep)
+
+    user_data = UserData.new(friends:           friends,
+                             sleep:             sleep,
+                             heartrate:         heartrates,
+                             last_nights_sleep: last_nights_sleep)
+
+    expect(user_data.todays_heartrate).to eq(88)
   end
 end
