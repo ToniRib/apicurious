218d85f49124bb84a2849c8ed9c4776e2f4fdefc
ToniRib <tonimarierib@gmail.com>
2016-02-16 19:16:45 -0700

    User has unique uid within context of provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :uid, uniqueness: { scope: :provider }
+
   def self.find_or_create_by_auth(auth)
     user = User.find_or_create_by(provider: auth[:provider],
                                   uid: auth[:uid])
