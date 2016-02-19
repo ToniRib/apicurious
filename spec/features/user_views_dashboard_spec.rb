865c24818b393a02e46420c087d7e9d52c7c06f7
ToniRib <tonimarierib@gmail.com>
2016-02-18 21:09:16 -0700

    Finish user views dashboard test

--- spec/features/user_views_dashboard_spec.rb
+++ spec/features/user_views_dashboard_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe "User Views Dashboard", type: :feature do
 
     expect(page).to have_content("Welcome, Toni Rib")
 
-    within ".friends" do
+    within ".friends-panel" do
       expect(page).to have_content("Carlos N.")
       expect(page).to have_content("Sharon R.")
       expect(page).to have_content("selby s.")
@@ -21,10 +21,36 @@ RSpec.describe "User Views Dashboard", type: :feature do
       expect(page).to have_content("Monique")
     end
 
-    within ".sleep-panel" do
+    within ".sleep-patterns-panel" do
       expect(page).to have_content("Time Asleep: 4 hours 20 minutes")
       expect(page).to have_content("Time Awake: 3 hours 10 minutes")
       expect(page).to have_content("Number of Awakenings: 20")
+    end
+
+    within ".heart-rate-panel" do
+      expect(page).to have_css("#heart-rate-chart")
+      expect(page).to have_content("Today: 83 bpm")
+    end
+
+    within ".sleep-panel" do
+      expect(page).to have_css("#sleep-chart")
+      expect(page).to have_content("3hr 39min")
+      expect(page).to have_content("awake 0x")
+      expect(page).to have_content("restless 7x")
+    end
+
+    within ".badge-gallery" do
+      expect(page).to have_content("30,000 steps in a day")
+      expect(page).to have_content("150 floors in a day")
+      expect(page).to have_content("1,600 lifetime miles")
+      expect(page).to have_content("4,000 lifetime floors")
+      expect(page).to have_content("-5 pounds")
+    end
+
+    within ".activity-panel" do
+      expect(page).to have_css("#daily-steps-chart")
+      expect(page).to have_css("#daily-floors-chart")
+      expect(page).to have_css("#daily-calories-chart")
     end
   end
 end
