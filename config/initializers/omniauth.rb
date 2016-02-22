13b2a57cfa3990c3febff731f9307074e9c4f2b9
ToniRib <tonimarierib@gmail.com>
2016-02-22 09:56:38 -0700

    Change redirect uri to heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :fitbit, ENV["consumer_key"],
                     ENV["consumer_secret"],
                     { :scope => "profile activity heartrate sleep social weight",
-                      :redirect_uri => "http://localhost:3000/auth/fitbit/callback" }
+                      :redirect_uri => "http://apicurious-fitbit.herokuapp.com/auth/fitbit/callback" }
 end
